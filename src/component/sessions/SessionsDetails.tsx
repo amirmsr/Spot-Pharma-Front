4f924f4dfcfbf06451f10f10d691fe1ed2c2dd5f
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-07-03 10:10:17 +0200

    inscrit au sessions

--- src/component/sessions/SessionsDetails.tsx
+++ src/component/sessions/SessionsDetails.tsx
@@ -26,8 +26,51 @@ interface Sessions {
 function Session() {
 
     const { sessionId } = useParams<{ sessionId: string }>();
+    
+    //fetch les inscrits
+    const { data: users } = useQuery("Users", async () => {
+    try {
+    const response = await fetch(`https://spot-pharma-api-bd00f8c1ff03.herokuapp.com/users`, {});
+    if (!response.ok) {
+        throw new Error("Failed to fetch sessions");
+    }
+    const data = await response.json();
+    return data;
+    } catch (err) {
+    throw new Error("An error occurred while fetching sessions");
+    }
+    });
 
-    console.log(sessionId)
+
+    // fetch les sessions
+    const { data: inscrits } = useQuery("Inscrits", async () => {
+    try {
+    const response = await fetch(`https://spot-pharma-api-bd00f8c1ff03.herokuapp.com/session_inscrit/${sessionId}`, {});
+    if (!response.ok) {
+        throw new Error("Failed to fetch sessions");
+    }
+    const data = await response.json();
+    return data;
+    } catch (err) {
+    throw new Error("An error occurred while fetching sessions");
+    }
+    });
+
+
+    // filtrer users 
+
+    const filterUsers = users.filter((user: { id: any; })=>{
+        const exists = inscrits.some((inscrit: { id_user: any; }) => inscrit.id_user === user.id)
+        return exists
+    })
+
+    filterUsers.map((filterUser: any)=>{
+        console.log(filterUser)
+    })
+   
+
+
+
 
 
   return (
