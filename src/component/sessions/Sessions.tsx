6bec8604dd5b264aa4488992807aec7d2b826836
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-07-05 11:19:21 +0200

    05/07

--- src/component/sessions/Sessions.tsx
+++ src/component/sessions/Sessions.tsx
@@ -157,7 +157,7 @@ function Session() {
   const filteredSessions = elements?.filter((session: { id: any; }) => userSessions.some((userSession: { id_session: any; }) => userSession.id_session === session.id));
 
 
-  console.log(filteredSessions.id_session)
+  console.log(filteredSessions.id)
 
 
 
