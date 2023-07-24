a274b111ff73131a886fcb9ac8c1f0f3451ff888
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-07-24 16:19:31 +0200

    24/07

--- src/component/accueil/Accueil.tsx
+++ src/component/accueil/Accueil.tsx
@@ -181,10 +181,10 @@ function Accueil(){
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide> <img src="../uploads/intervenant/intervenant2.jpg"  alt="" /></SwiperSlide>
+      <SwiperSlide> <img src="../uploads/intervenant/intervenant2.jpg"  alt="" /></SwiperSlide>
+      <SwiperSlide> <img src="../uploads/intervenant/intervenant2.jpg"  alt="" /></SwiperSlide>
+      <SwiperSlide> <img src="../uploads/intervenant/intervenant2.jpg"  alt="" /></SwiperSlide>
       ...
     </Swiper>
 
