f251a568beca5dec5a125cc285cd9f3feffd81bb
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-07-25 12:47:46 +0200

    25/07

--- src/component/accueil/Accueil.tsx
+++ src/component/accueil/Accueil.tsx
@@ -146,7 +146,7 @@ function Accueil(){
                 
             <br /><br />
             
-            <div style={{/* maxWidth:'1320px', */ margin:'0 auto'}}>
+            <div style={{maxWidth:'1320px', margin:'0 auto', backgroundColor:'#e5e7ea'}}>
                 <ScrollingCarousel>
                 <div className='col-md-4'>
                     <div className="sponsor">
