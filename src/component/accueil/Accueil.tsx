91eb4e805338e78583961c2493dae7a33fc01688
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-07-25 11:47:26 +0200

    25/07

--- src/component/accueil/Accueil.tsx
+++ src/component/accueil/Accueil.tsx
@@ -1,4 +1,5 @@
 import { useSnapCarousel } from 'react-snap-carousel';
+import { ScrollingCarousel } from '@trendyol-js/react-carousel';
 
 
 
@@ -142,34 +143,47 @@ function Accueil(){
 
             <br /><br />
             <ul
-      ref={scrollRef}
-      style={{
-        display: 'flex',
-        overflow: 'auto',
-        scrollSnapType: 'x mandatory'
-      }}
-    >
-      {Array.from({ length: 100 }).map((_, i) => (
-        <li
-          style={{
-            backgroundColor: 'aqua',
-            fontSize: '50px',
-            width: '250px',
-            height: '250px',
-            flexShrink: 0,
-            color: '#fff',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-        >
-          Item {i}
-        </li>
-      ))}
-    </ul>
-  
-  
-            <br></br><br></br>  <br></br><br></br>  <br></br><br></br>  <br></br>
+            ref={scrollRef}
+            style={{
+                display: 'flex',
+                overflow: 'auto',
+                scrollSnapType: 'x mandatory'
+            }}
+            >
+            {Array.from({ length: 100 }).map((_, i) => (
+                <li
+                style={{
+                    backgroundColor: 'aqua',
+                    fontSize: '50px',
+                    width: '250px',
+                    height: '250px',
+                    flexShrink: 0,
+                    color: '#fff',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+                >
+                Item {i}
+                </li>
+            ))}
+            </ul>
+
+                
+            <br /><br />
+
+            <ScrollingCarousel>
+            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
+            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
+            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
+            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
+            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
+            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
+            </ScrollingCarousel>,
+
+
+
+        <br></br><br></br>  <br></br><br></br>  <br></br><br></br>  <br></br>
         </div>
     )
 }
