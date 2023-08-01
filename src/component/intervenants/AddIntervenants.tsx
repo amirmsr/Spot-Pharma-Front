5783b81f1de7b2aecca89cd7b11e5ca6cb67d900
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-08-01 15:06:22 +0200

    31/07

--- src/component/intervenants/AddIntervenants.tsx
+++ src/component/intervenants/AddIntervenants.tsx
@@ -11,6 +11,16 @@ function AddIntervenants() {
     const navigate = useNavigate();
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
+    const [data, setData] = useState({
+        nom: "",
+        description: "",
+    });
+        
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setData((prevData) => ({ ...prevData, [e.target.name]: e.target.value }));
+    };
+    
+      
     //upload image
     const uploadImage = useMutation((formData: FormData) => {
         return fetch(`https://spot-pharma-api-bd00f8c1ff03.herokuapp.com/sessions/invite`, {
@@ -49,9 +59,14 @@ function AddIntervenants() {
     
     const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
         const formData = new FormData();
+
+        formData.append("nom", data.nom);
+        formData.append("description", data.description);
+
         if (selectedFile) {
-            formData.append("intervenant1", selectedFile);
+            formData.append("invite", selectedFile);
         }
+        console.log(formData)
         uploadImage.mutate(formData);
         e.preventDefault()
     }
@@ -65,6 +80,14 @@ function AddIntervenants() {
         <div>
             <Form onSubmit={handleSubmit} className="mb-3" style={{paddingBottom:'300px',paddingTop:'100px'}}>
                 <Form.Label>Ajouter un intervenant</Form.Label>
+
+                <p style={{color:'#7DBA33', fontSize:'1.5rem'}}>Nom</p>
+                <input type="text" name="nom"  placeholder='Nom' onChange={handleChange} />
+                <br />
+                <p style={{color:'#7DBA33', fontSize:'1.5rem'}}>Description</p>
+                <input type="text" name="description"  placeholder='Description' onChange={handleChange} />
+
+
                 <Form.Control type="file" id="image" name="image" onChange={handleChangeFile} />
                 <br></br> 
                 <button type="submit" disabled={isDisabled} style={{opacity: isDisabled ? 0.4 : 1}}> 
