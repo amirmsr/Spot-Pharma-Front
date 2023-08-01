1691efe166b3486b155d1e01ef0fa444369dd972
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-08-01 16:16:45 +0200

    31/07

--- src/component/intervenants/Intervenants.tsx
+++ src/component/intervenants/Intervenants.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 
@@ -11,10 +12,36 @@ export default function Intervenants(){
     }
     
     const navigate = useNavigate()
+    const [isAdmin, setIsadmin] = useState(false);
+    const token = localStorage.getItem("token");
 
-    const { data: elements, isLoading, isError } = useQuery("Sessions", async () => {
+    //get user data
+    const {data: user}= useQuery("userProfile", async ()=>{
+        if (!token){
+        throw new Error("token missing");
+        }
+        const response = await fetch ("https://spot-pharma-api-bd00f8c1ff03.herokuapp.com/home",{
+        headers: {
+            token: `${token}`,
+        }
+        })
+        if (!response.ok){
+        throw new Error("failed to fetch user profil")
+        }
+        const data = await response.json();
+        return data
+    })
+
+    useEffect(() => {
+        if (user?.role === 1) {
+        setIsadmin(true);
+        }
+    }, [user]);
+
+    //fetch les intervenants
+    const { data: elements, isLoading, isError } = useQuery("Invites", async () => {
         try {
-          const response = await fetch("https://spot-pharma-api-bd00f8c1ff03.herokuapp.com/invites", {});   //https://spot-pharma-api-bd00f8c1ff03.herokuapp.com/sessions
+          const response = await fetch("https://spot-pharma-api-bd00f8c1ff03.herokuapp.com/invites", {}); 
           if (!response.ok) {
             throw new Error("Failed to fetch invites");
           }
@@ -27,19 +54,19 @@ export default function Intervenants(){
 
     if (isLoading) {
         return <div>Chargement...</div>;
-      }
-      
-      if (isError) {
-        alert("erreur")
-      }
-      console.log(elements)
-      if (!elements || elements.length === 0) {
-        return <div>Aucun intervenant disponible</div>;
-      }
+    }
+    
+    if (isError) {
+    alert("erreur")
+    }
+    console.log(elements)
+    if (!elements || elements.length === 0) {
+    return <div>Aucun intervenant disponible</div>;
+    }
 
-      const handleClick=()=>{
-        navigate(`https://spot-pharma-front.vercel.app/addIntervenants`)
-      }
+    const handleClick=()=>{
+    navigate(`./addIntervenants`)
+    }
     
         
     
@@ -51,11 +78,12 @@ export default function Intervenants(){
             <div style={{paddingTop:'90px'}}>
             <h2  style={{ margin:"0", color:"#7DBA33"}}>Tous les</h2>
             <h3 style={{fontSize:"2rem", margin:"0"}}>Intervenants</h3>
-            </div> 
+            </div>             
             <br /><br />
-            <button className="btnMain2" onClick={() => handleClick()}>Ajouter un nouvel intervenant</button>
+            {isAdmin ? (
+                <button className="btnMain2" onClick={() => handleClick()}>Ajouter un nouvel intervenant</button>  
+            ):null}
             <br /><br /> 
-
             <div className="container">
                 <div className="row">
                     {elements?.slice(0.3).map((element:Intervenant)=>(
