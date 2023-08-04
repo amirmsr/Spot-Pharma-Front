fc3830f780391a1a15df387648d1089796eacbf7
Amir Mansour <amirmansour@MacBook-Pro-de-Amir.local>
2023-08-04 11:19:46 +0200

    04/08

--- src/component/sessions/Sessions.tsx
+++ src/component/sessions/Sessions.tsx
@@ -114,29 +114,34 @@ function Session() {
   });
 
 
-  function associerIntervenantsAuxSessions(sessions: Sessions[], intervenant_session: IntervenantSession[], intervenantsDetails: Intervenant[]) {
-    // Créer un objet de dictionnaire pour les détails des intervenants, indexé par leur ID
-    const intervenantsDict: { [id: number]: Intervenant } = {};
-    intervenantsDetails?.forEach(intervenant => {
-      intervenantsDict[intervenant.id] = intervenant;
-    });
-  
-    // Parcourir les sessions et associer les intervenants appropriés
-    const sessionsAvecIntervenants: Sessions[] = sessions?.map(session => {
+  function associerIntervenantsAuxSessions(
+    sessions: Sessions[],
+    intervenant_session: IntervenantSession[],
+    intervenantsDetails: Intervenant[]
+  ): Sessions[] {
+    const sessionsAvecIntervenants: Sessions[] = sessions.map(session => {
       const sessionIntervenants: Intervenant[] = [];
-      intervenant_session?.forEach(intervenantSession => {
+  
+      intervenant_session.forEach(intervenantSession => {
         if (
-          intervenantSession.id_session === session.id &&
-          intervenantsDict[intervenantSession.id_invite]
+          intervenantSession.id_session === session.id
         ) {
-          sessionIntervenants.push(intervenantsDict[intervenantSession.id_invite]);
+          const intervenant = intervenantsDetails.find(
+            intervenant => intervenant.id === intervenantSession.id_invite
+          );
+  
+          if (intervenant) {
+            sessionIntervenants.push(intervenant);
+          }
         }
       });
+  
       return { ...session, intervenants: sessionIntervenants };
     });
   
     return sessionsAvecIntervenants;
   }
+  
   
   // Utilisation de la fonction
   const sessionsAvecIntervenants: Sessions[] = associerIntervenantsAuxSessions(elements, intervSessions, intervSessions);
