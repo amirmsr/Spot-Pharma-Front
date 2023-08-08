ac8e7f2003d08c1c731f3d47ca4a48fb6a0db733
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-08-08 12:42:10 +0200

    08/08

--- src/component/sessions/Sessions.tsx
+++ src/component/sessions/Sessions.tsx
@@ -44,7 +44,7 @@ function Session() {
     if (!token){
       throw new Error("token missing");
     }
-    const response = await fetch ("http://localhost:3000/home",{
+    const response = await fetch ("https://spot-pharma-front.vercel.app/home",{
       headers: {
         token: `${token}`,
       }
@@ -72,7 +72,7 @@ function Session() {
   //fetch les intervenant 
   const { data: interv, } = useQuery("Intervenant", async () => {
     try {
-      const response = await fetch("http://localhost:3000/intervenants");  
+      const response = await fetch("https://spot-pharma-front.vercel.app/intervenants");  
       if (!response.ok) {
         throw new Error("Failed to fetch interv");
       }
@@ -86,7 +86,7 @@ function Session() {
   //fetch les intervenant des sessions
   const { data: intervSessions, } = useQuery("IantervSessions", async () => {
     try {
-      const response = await fetch(`http://localhost:3000/intervenants_session/`);  
+      const response = await fetch(`https://spot-pharma-front.vercel.app/intervenants_session/`);  
       if (!response.ok) {
         throw new Error("Failed to fetch interv");
       }
@@ -102,7 +102,7 @@ function Session() {
   // fetch les sessions
   const { data: elements, isLoading, isError } = useQuery("Sessions", async () => {
     try {
-      const response = await fetch("http://localhost:3000/sessions");  
+      const response = await fetch("https://spot-pharma-front.vercel.app/sessions");  
       if (!response.ok) {
         throw new Error("Failed to fetch sessions");
       }
@@ -154,7 +154,7 @@ function Session() {
     try {
       const token = localStorage.getItem("token");
       const headers = { token: `${token}` };
-      const response = await fetch("http://localhost:3000/session_inscrit", {  //http://localhost:3000/session_inscrit
+      const response = await fetch("https://spot-pharma-front.vercel.app/session_inscrit", {  //https://spot-pharma-front.vercel.app/session_inscrit
         method: 'POST',
         headers: {
           ...headers,
@@ -202,7 +202,7 @@ function Session() {
       const userId = user.id;
 
       const response = await fetch(
-        `http://localhost:3000/inscrit_session/${userId}`,
+        `https://spot-pharma-front.vercel.app/inscrit_session/${userId}`,
         {
           headers: {
             token: `${token}`,
@@ -279,7 +279,7 @@ function Session() {
   
 
   const handleDeleteInterv= async (id_sessions: number , id_intervenants: number)=>{
-    const response = await fetch(`http://localhost:3000/session_intervenants/${id_sessions}/${id_intervenants}`, {
+    const response = await fetch(`https://spot-pharma-front.vercel.app/session_intervenants/${id_sessions}/${id_intervenants}`, {
       method: 'DELETE',
     });
   
