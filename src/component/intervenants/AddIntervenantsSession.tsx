e32af1fd9fdf1eb550e0ef92749abb7bff5c3a7e
Amir Mansour <amirmansour@MacBook-Pro-de-Amir.local>
2023-08-11 16:54:38 +0200

    11/08

--- src/component/intervenants/AddIntervenantsSession.tsx
+++ src/component/intervenants/AddIntervenantsSession.tsx
@@ -160,7 +160,7 @@ function AddIntervenantsSession() {
             <center>
                 {intervSelected?.map((interv:Intervenant)=>(
                     <div>
-                        <img  alt="" src={"https://bcombrun.com/Spot-Pharma-Image/Intervenant/" + interv.image}/>
+                        <img width={'10%'}  alt="" src={"https://bcombrun.com/Spot-Pharma-Image/Intervenant/" + interv.image}/>
                     </div>
                 ))}
             </center>
