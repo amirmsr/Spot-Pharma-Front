bc240528f56caf38c7c2801755a8787c8e408416
Amir Mansour <amirmansour@MacBook-Pro-de-Amir.local>
2023-08-11 12:53:16 +0200

    11/08

--- src/component/sponsors/Sponsors.tsx
+++ src/component/sponsors/Sponsors.tsx
@@ -41,7 +41,7 @@ export default function Sponsors(){
     }, [user]);
 
     //fetch les sponsors
-    const { data: elements, isLoading, isError } = useQuery("Sponsors", async () => {
+    const { data: elements, isError } = useQuery("Sponsors", async () => {
         try {
           const response = await fetch("https://spot-pharma-api-bd00f8c1ff03.herokuapp.com/sponsors"); 
           if (!response.ok) {
@@ -54,17 +54,12 @@ export default function Sponsors(){
         }
     });
 
-    if (isLoading) {
-        return;
-    }
-    
+
     if (isError) {
     alert("erreur")
     }
     console.log(elements)
-    if (!elements || elements.length === 0) {
-    return ;
-    }
+
 
     const handleClick=()=>{
     navigate(`addSponsors`)
