117209c57266577332b7ec38626b5d55ef7d9592
Amir Mansour <amirmansour@MacBook-Pro-de-Amir.local>
2023-08-16 11:25:06 +0200

    16/08

--- src/component/accueil/Accueil.tsx
+++ src/component/accueil/Accueil.tsx
@@ -52,15 +52,13 @@ function Accueil(){
             <br /><br />
 
             {/*  slider sponsors */}
-            <div style={{textAlign:'center', marginBottom:'30px', /* backgroundColor:"rgb(255, 255, 255)", boxShadow:' 0px 1px 40px 14px rgba(0, 0, 0, 0.07)',borderRadius:'15px' */}}>
+            <div style={{textAlign:'center', marginBottom:'30px',backgroundColor:"rgb(255, 255, 255)", boxShadow:' 0px 1px 40px 14px rgba(0, 0, 0, 0.07)',borderRadius:'15px' /* backgroundColor:"rgb(255, 255, 255)", boxShadow:' 0px 1px 40px 14px rgba(0, 0, 0, 0.07)',borderRadius:'15px' */}}>
             <div className="container" >
                 <Slider {...settings} >
                 {Sponsors.map((item) => (
                     <div key={item.id} >
                     <img src={item.src} alt={item.alt} style={{margin:'0 auto',width:'100%', height:'100%'}} />
                     <br />
-                    <h3>{item.title}</h3>
-                    <p>{item.description}</p>
                     </div>
                 ))}
                 </Slider>
