273c1aca24d5e2e2235b0be2ac189bc5be2d7d75
Amir Mansour <amirmansour@mbp-de-amir.home>
2023-09-13 11:17:14 +0200

    actu

--- src/component/accueil/Accueil.tsx
+++ src/component/accueil/Accueil.tsx
@@ -85,7 +85,47 @@ function Accueil(){
                     <br /><br /><br />
             
 
-            <br /><br />            
+            <br /><br /> 
+            <div style={{backgroundImage: `url("./hexa.png")`, backgroundRepeat:'none', backgroundSize:'1100px', backgroundPosition:'center'}}>
+                <h2  style={{ margin:"0", color:"#7DBA33"}}>Tous les</h2>
+                <h3 style={{fontSize:"2rem", margin:"0", color:"#004651"}}>Actualités</h3>
+                <br /><br />
+
+                {/*  actualité */}
+                <div style={{textAlign:'center', marginBottom:'30px' /* backgroundColor:"rgb(255, 255, 255)", boxShadow:' 0px 1px 40px 14px rgba(0, 0, 0, 0.07)',borderRadius:'15px' */}}>
+                    <div className="container">
+                        <div className="row">
+                            <div  className="col-sm">
+                                <div className="session" style={{paddingTop:'50px',marginBottom: '10px',}}>
+                                    <div>
+                                        <div style={{ height: '100px', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                                            <img style={{objectFit: 'contain',width: '100%',height: '100%',}} src="https://bcombrun.com/Spot-Pharma-Image/LogoSponsors/Group%2091.png"  alt="" />                 
+                                        </div> 
+                                        <br /> 
+                                        <p></p>
+                                    </div>                        
+                                </div>
+                            </div>
+                            <div  className="col-sm">
+                                <div className="session" style={{paddingTop:'50px',marginBottom: '10px',}}>
+                                    <div>
+                                        <div style={{ height: '100px', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                                            <img style={{objectFit: 'contain',width: '100%',height: '100%',}} src="https://bcombrun.com/Spot-Pharma-Image/LogoSponsors/sfspo.png" alt="" />                 
+                                        </div> 
+                                        <br /> 
+                                        <p></p>
+                                    </div>                        
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>   
+              
+
+
+            <br /><br /><br /><br />
+
 
             <h2  style={{ margin:"0", color:"#7DBA33"}}>Tous les</h2>
             <h3 style={{fontSize:"2rem", margin:"0", color:"#004651"}}>Partenaires</h3>
