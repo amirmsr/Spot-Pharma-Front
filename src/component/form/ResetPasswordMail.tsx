04051f96bc0b6a184480487bdae86db9c15d62d8
Your Name <n.barbier001@gmail.com>
2023-09-15 18:45:38 +0200

    fix messasge mail reset password

--- src/component/form/ResetPasswordMail.tsx
+++ src/component/form/ResetPasswordMail.tsx
@@ -52,7 +52,7 @@ export default function ResetPasswordMail() {
       <form className="addSessionForm" onSubmit={handleSubmit} >
         <div className="add" >
           <br /><br /><br />
-          <h1 style={{color:'white'}}>Écrivez votre nouveaux mot de passe</h1>
+          <h1 style={{color:'white'}}>Écrivez votre nouveau mot de passe</h1>
           <br />
           <br />
           <div className="container">
