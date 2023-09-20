1024fbda136fdf3160f12574ec68da9e1741c631
Your Name <n.barbier001@gmail.com>
2023-09-20 15:12:24 +0200

    Ajout du responsive sur le formulaire d'inscription

--- src/component/form/Inscription.tsx
+++ src/component/form/Inscription.tsx
@@ -45,58 +45,65 @@ export default function Inscription(){
     }
   }
 
-
-
-
-
-
   return (
-    
   <div className="loginBg">
-    <div className="container">
-      <form style={{ paddingBottom: '100px', padding: '100px' }} onSubmit={handleSubmit}>
-        <div className="add" style={{ backgroundColor: 'white', borderRadius: '15px' }}>
-          <br /><br /><br />
+    <div className="container w-md-75 p-5 text-center">
+      <form onSubmit={handleSubmit}>
+        <div className="card mt-5 mb-5 p-5">
           <h1 style={{ color: '#1A4550', textAlign: 'center' }}>Inscription</h1>
-          <br /><br />
-          <div className="form-group">
-            <label htmlFor="name" style={{ color: '#1A4550', fontSize: '1.5rem' }}>Nom</label>
-            <center><input style={{width:'30%'}} className='form-control' type="text" id="name" name="name" placeholder='Nom' onChange={handleChange} required /></center>
-          </div>
-          <br /><br />
-          <div className="form-group">
-            <label htmlFor="prenom" style={{ color: '#1A4550', fontSize: '1.5rem' }}>Prénom</label>
-            <center><input style={{width:'30%'}} className='form-control' type="text" id="prenom" name="prenom" placeholder='Prénom' onChange={handleChange} required /></center>
-          </div>
-          <br /><br />
-          <div className="form-group">
-            <label htmlFor="email" style={{ color: '#1A4550', fontSize: '1.5rem' }}>Email</label>
-            <center><input type="text" style={{width:'30%'}} id="email" name="email" className='form-control' placeholder='Email' onChange={handleChange} required pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" /></center>
-          </div>
-          <br /><br />
-          <p style={{ color: '#1A4550', fontSize: '1.5rem', textAlign: 'center' }}>Fonction</p>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <select style={{ width: '30%' }} name="fonction" onChange={handleSelectChange} value={data.fonction} required>
-              <option value="">Sélectionnez une fonction</option>
-              {fonctionOptions.map((option, index) => (
-                <option key={index} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <div style={{ marginTop: '30px', width: '60%' }}>
-              <label htmlFor="password" style={{ color: '#1A4550', fontSize: '1.5rem' }}>Mot de passe</label>
-             <center><input style={{width:'30%'}} className='form-control' type="password" id="password" name="password" placeholder="Mot de passe" onChange={handleChange} required /></center>
+
+          <div className="row justify-content-center px-md-5">
+            <div className="col-md-4 text-center py-3">
+              <div className="form-group">
+                <label htmlFor="name">Nom</label>
+                <input className='form-control' type="text" id="name" name="name" placeholder='Nom' onChange={handleChange} required />
+              </div>
+            </div>
+            <div className="col-md-4 text-center py-3">
+              <div className="form-group">
+                <label htmlFor="prenom">Prénom</label>
+                <input className='form-control' type="text" id="prenom" name="prenom" placeholder='Prénom' onChange={handleChange} required />
+              </div>
             </div>
           </div>
-          <br /><br />
+
+          <div className="row justify-content-center px-md-5 py-3">
+            <div className="col-md-8">
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input className='form-control' id="email" name="email" placeholder='Email' onChange={handleChange} required pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" />
+              </div>
+            </div>
+          </div>
+
+          <div className="row justify-content-center px-md-5 py-3">
+            <div className="col-md-8">
+              <label>Fonction</label>
+              <select className="form-select" name="fonction" onChange={handleSelectChange} value={data.fonction} required>
+                <option value="">Sélectionnez une fonction</option>
+                {fonctionOptions.map((option, index) => (
+                    <option key={index} value={option.value}>
+                      {option.label}
+                    </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <div className="row justify-content-center px-md-5 py-3">
+            <div className="col-md-8">
+              <div className="form-group">
+                <label htmlFor="password">Mot de passe</label>
+                <input className='form-control' type="password" id="password" name="password" placeholder="Mot de passe" onChange={handleChange} required />
+              </div>
+            </div>
+          </div>
+
           <div style={{ display: 'flex', justifyContent: 'center' }}>
             <button type="submit" className='btnMain2 btn-block'>S'inscrire</button>
           </div>
-          <br /><br />
         </div>
+
       </form>
     </div>
   </div>
